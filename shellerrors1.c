99c7e67588df961a80e97847b8ef5532c5082cb2
Nigel Omondi <nigelomondi62@gmail.com>
2023-04-28 06:43:17 +0300

    errors 1

--- shellerrors1.c
+++ shellerrors1.c
@@ -1,58 +1,131 @@
 #include "shell.h"
+
 /**
- * print_error - function to print the error message
- * @info: struct
- * @estr: string
- * Return: -1 on an error
+ * _erratoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ *       -1 on error
+ */
+int _erratoi(char *s)
+{
+	int i = 0;
+	unsigned long int result = 0;
+
+	if (*s == '+')
+		s++;  /* TODO: why does this make main return 255? */
+	for (i = 0;  s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
+	return (result);
+}
+
+/**
+ * print_error - prints an error message
+ * @info: the parameter & return info struct
+ * @estr: string containing specified error type
+ * Return: 0 if no numbers in string, converted number otherwise
+ *        -1 on error
  */
 void print_error(info_t *info, char *estr)
 {
 	_eputs(info->fname);
-	_eput(": ");
+	_eputs(": ");
 	print_d(info->line_count, STDERR_FILENO);
 	_eputs(": ");
 	_eputs(info->argv[0]);
 	_eputs(": ");
 	_eputs(estr);
 }
+
 /**
- * convert_number - converts a number
- * @num: the number
- * @base: thr base number
- * @flags: arguments
- * Return: return the string
+ * print_d - function prints a decimal (integer) number (base 10)
+ * @input: the input
+ * @fd: the filedescriptor to write to
+ *
+ * Return: number of characters printed
+ */
+int print_d(int input, int fd)
+{
+	int (*__putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int _abs_, current;
+
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (_abs_ / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	count++;
+
+	return (count);
+}
+
+/**
+ * convert_number - converter function, a clone of itoa
+ * @num: number
+ * @base: base
+ * @flags: argument flags
+ *
+ * Return: string
  */
 char *convert_number(long int num, int base, int flags)
 {
-	char sign = 0;
-	unsigned long n = num;
-	char *ptr;
-	static char buffer[50];
 	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
 
-	if (sign)
-		*--ptr = sign;
-
-	if (!(flags & CONVVERT_UNSIGNED) && num < 0)
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
 	{
 		n = -num;
 		sign = '-';
+
 	}
 	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
 	ptr = &buffer[49];
 	*ptr = '\0';
 
-	do {
+	do	{
 		*--ptr = array[n % base];
 		n /= base;
 	} while (n != 0);
 
+	if (sign)
+		*--ptr = sign;
 	return (ptr);
 }
+
 /**
- * remove_comments - function to rpeplace comments
- * @buf: address of the string
- * Return: 0
+ * remove_comments - function replaces first instance of '#' with '\0'
+ * @buf: address of the string to modify
+ *
+ * Return: Always 0;
  */
 void remove_comments(char *buf)
 {
@@ -64,70 +137,4 @@ void remove_comments(char *buf)
 			buf[i] = '\0';
 			break;
 		}
-}
-/**
- * _erratoi - function to convert a string to an integer
- * @s: string
- * Return: -1 if an error occurs
- */
-int _erratoi(char *s)
-{
-	int i = 0;
-	unsigned long int result = 0;
-
-	if (*s == '+')
-		s++;
-	for (i = 0; s[i] !+'\0'; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result = result + (s[i] - '0');
-			if (result > INT_MAX)
-				return (-1);
-		}
-		else
-			return (-1);
-	}
-	return (result);
-}
-/**
- * print_d - prints a decimal integer
- * @input: the input
- * @fd: file descriptor
- * Return: numbers printed
- */
-int print_d(int input, int fd)
-{
-	int i, count = 0;
-	unsigned int _abs_, current;
-	int (*__putchar)(char) = _putchar;
-
-	for (i = 1000000000; i > 1; i /= 10)
-	{
-		if (_abs_ / i)
-		{
-			__putchar('0' + current / i);
-			count++;
-		}
-		current = current % i;
-	}
-
-	if (fd == STDERR_FILENO)
-		__putchar = _eputchar;
-
-	if (input < 0)
-	{
-		_abs_ = -input;
-		__putchar('-');
-		count++
-	}
-	else
-		_abs_ = input;
-	current = _abs_;
-
-	__putchar('0' + current);
-	count++;
-
-	return (count);
 }
